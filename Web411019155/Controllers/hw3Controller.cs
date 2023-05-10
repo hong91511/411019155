679183eb449559961ec69cbcdff0a78df41d63d5
hong91511 <b0931368278@gmail.com>
2023-05-10 20:29:37 +0800

    hw3

---
+++ Web411019155/Controllers/hw3Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411019155.Controllers
+{
+    public class hw3Controller : Controller
+    {
+        // GET: hw3
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult hw3()
+        {
+            return View();
+        }
+    }
+}
