ca0b1a7ac68df71b09d49cb8275b1676c383ca23
hong91511 <b0931368278@gmail.com>
2023-05-31 20:47:06 +0800

    0531-2

--- Web411019155/Models/Partials/Student.cs
+++ Web411019155/Models/Partials/Student.cs
@@ -19,7 +19,7 @@ namespace Web411019155.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5,MinimumLength=2, ErrorMessage ="請輸入2~5個字")]
-        public string Name{get; set; }
+        public string UserName{get; set; }
 
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
--- Web411019155/Models/Student.cs
+++ Web411019155/Models/Student.cs
@@ -15,7 +15,7 @@ namespace Web411019155.Models
     public partial class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; set; }
         public string Number { get; set; }
         public string Email { get; set; }
         public bool IsActive { get; set; }
